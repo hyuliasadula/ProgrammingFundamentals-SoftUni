f0fa7bc0f1dd8b001a44da5f4e6fc61386a80b9e
hyuliasadula <huliya1401@gmail.com>
2023-10-18 00:54:32 +0300

    added 1-4 method exercises

---
+++ Lections/04_Methods/01_SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01_SignOfIntegerNumbers
+{
+    internal class Program
+    {
+
+        /*
+         A single integer is given, create a method that checks if the given number is positive, negative, or zero. As a result
+                        print:
+                        • For positive number: "The number {number} is positive. "
+                        • For negative number: "The number {number} is negative. "
+                        • For zero number: "The number {number} is zero. "
+
+        Input            Output
+        2                The number 2 is positive.
+        -9               The number -9 is negative.
+
+         */
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            CheckNumber(number);
+        }
+
+        static void CheckNumber(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
---
+++ Lections/04_Methods/03_Calculations/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _03_Calculations
+{
+    internal class Program
+    {
+        /*Create a program that receives three lines of input:
+        • On the first line – a string – "add", "multiply", "subtract", "divide".
+        • On the second line – a number.
+        • On the third line – another number.
+        You should create four methods (for each calculation) and invoke the corresponding method depending on the
+        command. The method should also print the result (needs to be void).
+        
+         
+         Input            Output
+        subtract
+        5
+        4
+                            1
+
+        divide
+        8
+        4
+                            2
+        */
+        static void Main(string[] args)
+        {
+            string arithmeticSigns = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            AritmeticsSum(arithmeticSigns, firstNumber, secondNumber);
+
+
+            
+        }
+        static void AritmeticsSum(string arithmeticSigns, int firstNumber, int secondNumber)
+        {
+            if (arithmeticSigns == "add")
+            {
+                Add(firstNumber, secondNumber);
+            }
+            else if (arithmeticSigns == "subtract")
+            {
+                Subtract(firstNumber, secondNumber);
+            }
+            else if (arithmeticSigns == "multiply")
+            {
+                Multiply(firstNumber, secondNumber);
+            }
+            else if (arithmeticSigns == "divide")
+            {
+                Divide(firstNumber, secondNumber);
+            }
+        }
+        static void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+
+        static void Subtract(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine(result);
+        }
+
+        static void Multiply(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine(result);
+        }
+        static void Divide(int a, int b)
+        {
+           
+                int result = a / b;
+                Console.WriteLine(result);
+            
+        }
+
+    }
+}
